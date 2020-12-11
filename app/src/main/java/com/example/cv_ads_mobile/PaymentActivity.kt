c284b7fede4550b861e3a1fbe3826c4a3d6eb968
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-12-12 00:50:22 +0200

    Payment implemented.

--- app/src/main/java/com/example/cv_ads_mobile/PaymentActivity.kt
+++ app/src/main/java/com/example/cv_ads_mobile/PaymentActivity.kt
@@ -1,23 +1,28 @@
 package com.example.cv_ads_mobile
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.example.cv_ads_mobile.services.PersistentStorage
+import okhttp3.*
+import java.io.IOException
 
 class PaymentActivity : AppCompatActivity() {
     private val persistentStorage: PersistentStorage = PersistentStorage(this)
+    private val httpClient = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
 
-        updatePaymentAmount()
+        getPaymentAmount()
 
         findViewById<Button>(R.id.withdrawButton).setOnClickListener{
-            withdraw()
+            withdraw(this)
         }
     }
 
@@ -35,28 +40,73 @@ class PaymentActivity : AppCompatActivity() {
         }
     }
 
-    private fun updatePaymentAmount() {
-        val paymentAmount = getPaymentAmount()
+    private fun getPaymentAmount() {
+        val url = getString(R.string.api_base_url) + getString(R.string.api_get_payment_amount_url)
 
-        val paymentAmountTextView = findViewById<TextView>(R.id.paymentInDollarsTextView)
-        paymentAmountTextView.text = paymentAmount.toString() + "$"
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Accept-Language", persistentStorage.getCurrentLanguage())
+            .addHeader("Authorization", "Bearer " + persistentStorage.getAccessToken())
+            .build()
+
+        httpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("[*] Request error: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseText = response.body()?.string()
+                println("[*] Get payment amount response: $responseText")
+
+                if (response.isSuccessful) {
+                    runOnUiThread {
+                        val paymentAmountTextView = findViewById<TextView>(
+                            R.id.paymentInDollarsTextView)
+                        paymentAmountTextView.text = "$responseText $"
+                    }
+                } else {
+                    println("[*] Request error: ${response.code()}")
+                }
+            }
+        })
     }
 
-    private fun getPaymentAmount(): Int {
-        // TODO: implement
-        return 10
-    }
+    private fun withdraw(context: Context) {
+        val url = getString(R.string.api_base_url) + getString(R.string.api_withdraw_url)
 
-    private fun withdraw() {
-        // TODO: implement
+        val request = Request.Builder()
+            .url(url)
+            .method("POST", RequestBody.create(null, ""))
+            .addHeader("Accept-Language", persistentStorage.getCurrentLanguage())
+            .addHeader("Authorization", "Bearer " + persistentStorage.getAccessToken())
+            .build()
 
-        val message = getLocalizedSuccessMessage()
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        updatePaymentAmount()
+        httpClient.newCall(request).enqueue(object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("[*] Request error: ${e.message}")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseText = response.body()?.string()
+                println("[*] Withdraw response: $responseText")
+
+                if (response.isSuccessful) {
+                    runOnUiThread{
+                        val message = getLocalizedSuccessMessage()
+                        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+                        getPaymentAmount()
+                    }
+                } else {
+                    println("[*] Request error: ${response.code()}")
+                }
+            }
+        })
+
+
     }
 
     private fun getLocalizedSuccessMessage(): String {
-        var message = "Withdrawed successfully"
+        var message = "Withdraw succeed"
         if (persistentStorage.getCurrentLanguage() == getString(R.string.shared_preferences_language_ua_key)) {
             message = "Гроші успішно знято!"
         }
